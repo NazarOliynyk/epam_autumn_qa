594c90cfaa617dff3e102bdc88e8754bb572940f
Your Name <nazar_lw@ukr.net>
2019-11-13 14:56:09 +0200

    innitial commit

--- src/test/java/HardCodedTests.java
+++ src/test/java/HardCodedTests.java
@@ -1,8 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,24 +14,41 @@ public class HardCodedTests {
 
     @Test(description = "Verify 'Trainings' search works properly with searching in 'Skills'")
     public void verifyTrainingsSearchWorksProperlyForSkills() {
+
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); //DOWNLOAD DRIVER !
+
         WebDriver driver =new ChromeDriver();
+
         WebDriverWait wait=new WebDriverWait(driver, 20);
+
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://training.by/#/Home");
 
-        WebElement signInButton = driver.findElement(By.xpath("//p[@class='heatytder-auth__signin']//span"));
+        WebElement signInButton = driver.findElement(By.xpath("//p[@class='header-auth__signin']//span"));
         signInButton.click();
+
         WebElement mailInput = driver.findElement(By.id("signInEmail"));
         mailInput.sendKeys("ivanhorintest@gmail.com");
+
         WebElement passwordInput = driver.findElement(By.id("signInPassword"));
         passwordInput.sendKeys("ivanhorintestPassword");
+
         WebElement signIn = driver.findElement(By.className("popup-reg-sign-in-form__sign-in"));
         signIn.click();
+
         WebElement expandSkillsArrow = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.className("filter-toggle__arrow-icon")));
-        expandSkillsArrow.click();
+
+            expandSkillsArrow.click();
+
+        WebElement byLocationsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[contains(text(),'By locations')]")));
+        byLocationsButton.click();
+
+        WebElement locationsSearchInput = driver
+                .findElement(By.xpath("//input[@name='training-filter-input']"));
+        locationsSearchInput.sendKeys("Minsk");
 
         WebElement bySkillsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//div[contains(text(),'By skills')]")));
@@ -41,19 +58,209 @@ public class HardCodedTests {
                 .findElement(By.xpath("//input[@name='training-filter-input']"));
         skillsSearchInput.sendKeys("Java");
 
+
+
         WebElement javaCheckbox = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//label[contains(.,'Java')]//span")));
         javaCheckbox.click();
 
-        WebElement collapseSkillsArrow = driver.findElement(By.xpath("//div[@class='filter-toggle__arrow-icon arrow-icon-rotate']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,300)");
+
+        WebElement collapseSkillsArrow = driver.findElement(By.className("filter-toggle__arrow-icon"));
         collapseSkillsArrow.click();
 
         List<WebElement> skillsSearchResultsList = driver.
                 findElements(By.xpath("//div[@class='training-list__container training-list__desktop']//a"));
+
         skillsSearchResultsList.forEach(element-> Assert.assertTrue(element.getText().contains("JAVA"),
                 String.format("Element %s does not contain 'Java' word.",element)));
+
         driver.quit();
 
-        //ADD OTHER STEPS
     }
+
+    @Test(description = "Verify logination with incorrect credentials")
+    public void verifyIncorrectCredentials() {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver =new ChromeDriver();
+
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://training.by/#/Home");
+
+        WebElement signInButton = driver.findElement(By.xpath("//p[@class='header-auth__signin']//span"));
+        signInButton.click();
+
+        WebElement mailInput = driver.findElement(By.id("signInEmail"));
+        mailInput.sendKeys("ivanhorintest@gmail.com");
+
+        WebElement passwordInput = driver.findElement(By.id("signInPassword"));
+        passwordInput.sendKeys("ivanhorintestWRONGPassword");
+
+        WebElement signIn = driver.findElement(By.className("popup-reg-sign-in-form__sign-in"));
+        signIn.click();
+
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector(" div.popup__error-message.ng-binding")));
+        System.out.println("errorMessage.getText(): "+errorMessage.getText());
+        Assert.assertEquals("Ошибка авторизации. Пожалуйста, попробуйте еще раз.", errorMessage.getText());
+
+
+        driver.quit();
+
+    }
+
+    @Test(description = "Verify 'Trainings' search works properly with searching in 'News'")
+    public void verifyNewsPageMaterialSection() {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); //DOWNLOAD DRIVER !
+
+        WebDriver driver =new ChromeDriver();
+
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://training.by");
+
+        WebElement signInButton = driver.findElement(By.xpath("//p[@class='header-auth__signin']//span"));
+        signInButton.click();
+
+        WebElement mailInput = driver.findElement(By.id("signInEmail"));
+        mailInput.sendKeys("ivanhorintest@gmail.com");
+
+        WebElement passwordInput = driver.findElement(By.id("signInPassword"));
+        passwordInput.sendKeys("ivanhorintestPassword");
+
+        WebElement signIn = driver.findElement(By.className("popup-reg-sign-in-form__sign-in"));
+        signIn.click();
+
+        WebElement newsButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("a[href='/#!/News']")));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", newsButton);
+
+
+        // start from success stories because News is open by default
+
+        WebElement successStoriesButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class=\"tab-nav\"]/div/div[2]/span")));
+
+
+        try {
+            Thread.sleep(1500);
+            successStoriesButton.click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement materialsButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class=\"tab-nav\"]/div/div[3]/span")));
+
+        try {
+            Thread.sleep(1500);
+            materialsButton.click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement videosButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class=\"tab-nav\"]/div/div[4]/span")));
+
+        try {
+            Thread.sleep(1500);
+            videosButton.click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement newsBigButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("div.tab-nav > div > div:nth-child(1) > span")));
+
+        try {
+            Thread.sleep(1500);
+            newsBigButton.click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        materialsButton.click();
+        List<WebElement> skillsSearchResultsList = driver.
+                findElements(By.cssSelector("div.news-page-item> div.news-page-item__title > a"));
+
+        skillsSearchResultsList.forEach(element->
+                System.out.println(element.getText())
+        );
+
+        skillsSearchResultsList.forEach(element->
+                Assert.assertTrue(element.getText().contains("materials")|| element.getText().contains("Useful")
+                ||element.getText().contains("Materials"))
+        );
+
+        driver.quit();
+
+    }
+
+    @Test(description = "Verify 'Trainings' search works properly with searching in 'Locations'")
+    public void verifyLocationsSection() {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); //DOWNLOAD DRIVER !
+
+        WebDriver driver =new ChromeDriver();
+
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+        Wait waitWithoutException = new FluentWait(driver)
+                .ignoring(NoSuchElementException.class, StaleElementReferenceException.class);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://training.by");
+
+        WebElement signInButton = driver.findElement(By.xpath("//p[@class='header-auth__signin']//span"));
+        signInButton.click();
+
+        WebElement mailInput = driver.findElement(By.id("signInEmail"));
+        mailInput.sendKeys("ivanhorintest@gmail.com");
+
+        WebElement passwordInput = driver.findElement(By.id("signInPassword"));
+        passwordInput.sendKeys("ivanhorintestPassword");
+
+        WebElement signIn = driver.findElement(By.className("popup-reg-sign-in-form__sign-in"));
+        signIn.click();
+
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+
+        WebElement trainingListButton = (WebElement) waitWithoutException.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("a[href='/#!/TrainingList']")));
+
+        executor.executeScript("arguments[0].click();", trainingListButton);
+
+
+        executor.executeScript("window.scrollBy(0,1650)");
+
+
+        WebElement searchTrainingArrow = (WebElement) waitWithoutException.until(ExpectedConditions
+                .visibilityOfElementLocated(By.className("filter-toggle-mobile__arrow-icon")));
+
+       searchTrainingArrow.click();
+
+//        try {
+//            executor.executeScript("arguments[0].click();", searchTrainingArrow);
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+                    driver.quit();
+
+
+
+    }
+
 }
+
